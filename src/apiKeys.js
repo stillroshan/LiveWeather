f28224a53d7c8f3c2bc6a27c70668802649598e8
Roshan Yadav <roshan643yadav@gmail.com>
2024-06-26 19:29:58 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{88a376bc7f106816c1001edec5119778}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
